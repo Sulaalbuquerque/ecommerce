91669551253b4c45dd741512bdba0a9b1ca28bf8
S L Albuquerque <devsulaalbuquerque@gmail.com>
2022-04-25 13:35:30 -0300

    -

--- src/pages/Home/Main/styles.js
+++ src/pages/Home/Main/styles.js
@@ -1,16 +1,9 @@
 import styled from 'styled-components'
 
 export const ContainerMain = styled.main`
-
-    margin: 0 4.5rem;
     padding: 0 2rem;
 
-    @media (max-width: 770px) {
-        margin: 0;
+    @media (max-width: 800px) {
         padding: 0;
-    }
-
-    @media (min-width: 990px) {
-        margin: 0 2rem;
     }
 `
